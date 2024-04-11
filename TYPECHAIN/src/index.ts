5b4b1d8c80eaa092a57063d5c4148e84f3bd0f8b
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2024-04-11 14:48:27 +0900

    Declaration File

--- TYPECHAIN/src/index.ts
+++ TYPECHAIN/src/index.ts
@@ -1,6 +1,16 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return 'hi';
-  }
-}
+import { init, exit } from 'myPackage';
+
+init({
+  url: 'true',
+});
+
+exit(1);
+
+localStorage.clear;
+
+// class Block {
+//   constructor(private data: string) {}
+//   static hello() {
+//     return 'hi';
+//   }
+// }
